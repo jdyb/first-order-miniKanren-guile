bdc2f140954813054540a4cbfbe78cde79a1e404
Jon Dybeck <jon.dybeck@liu.se>
2021-11-05 16:30:42 +0100

    Add assf implementation

---
+++ util.scm
@@ -0,0 +1,15 @@
+
+(define-module (util)
+  #:export (assf))
+
+(define (assf f al)
+  (if (procedure? f)
+      (if (and (pair? al) (pair? (car al)))
+          (if (f (caar al))
+              (cdar al)
+              (assf f (cdr al)))
+          (if (null? al)
+              #f
+              (error 'assf-bad-association-list al)))
+      (error 'assf-expected-procedure f)))
+
