04083a2eb28544a7de46c204216813b37bb6980f
Jon Dybeck <jon.dybeck@liu.se>
2021-11-05 16:58:13 +0100

    Fix reference to pause

--- mk-syntax.scm
+++ mk-syntax.scm
@@ -30,7 +30,7 @@
   (syntax-rules ()
     ((_ (x ...) g0 gs ...)
      (let ((goal (fresh (x ...) (== (list x ...) initial-var) g0 gs ...)))
-       (pause empty-state goal)))))
+       (mk-pause empty-state goal)))))
 (define (stream-take n s)
   (if (eqv? 0 n) '()
     (let ((s (mature s)))
