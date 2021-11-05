c34901f78ff4f73d220326c816dafe67b18d6740
Jon Dybeck <jon.dybeck@liu.se>
2021-11-05 17:09:25 +0100

    Translate mk-fo

--- mk-fo.scm
+++ mk-fo.scm
@@ -2,8 +2,7 @@
 (define-module (mk-fo)
   #:use-module (common)
   #:use-module (microk-fo)
-  #:re-export (==)
-  #:export (define-relation fresh conde query run run* stream-take conj*
+  #:re-export (== mk-pause)
+  #:export (fresh define-relation conde query run run* stream-take conj*
                disj*))
-(== 1 1)
 (include "mk-syntax.scm")
