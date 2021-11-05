a89250a93ab1c13cf54f639ca880aa11e157b020
Jon Dybeck <jon.dybeck@liu.se>
2021-11-05 16:59:03 +0100

    Translate mk-fo

--- mk-fo.scm
+++ mk-fo.scm
@@ -1,19 +1,9 @@
-#lang racket
-(provide
-  (all-from-out "common.rkt")
-  (all-from-out "microk-fo.rkt")
-  ==
 
-  define-relation
-  fresh
-  conde
-  query
-  run
-  run*
-
-  stream-take
-  conj*
-  disj*
-  )
-(require "common.rkt" "microk-fo.rkt")
-(include "mk-syntax.rkt")
+(define-module (mk-fo)
+  #:use-module (common)
+  #:use-module (microk-fo)
+  #:re-export (==)
+  #:export (define-relation fresh conde query run run* stream-take conj*
+               disj*))
+(== 1 1)
+(include "mk-syntax.scm")
