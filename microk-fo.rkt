604b5321ea7d7940a7562b0417fdc3a9bfd5ab5f
Jon Dybeck <jon.dybeck@liu.se>
2021-11-05 16:42:57 +0100

    Translate microk-fo

--- microk-fo.rkt
+++ microk-fo.rkt
@@ -1,27 +1,54 @@
-#lang racket
-(provide
-  (all-from-out "common.rkt")
-  (struct-out disj)
-  (struct-out conj)
-  (struct-out relate)
-  (struct-out ==)
-  (struct-out mplus)
-  (struct-out bind)
-  (struct-out pause)
-  step
-  mature
-  mature?)
 
-(require "common.rkt")
+(define-module (microk-fo)
+  #:use-module (srfi srfi-9)
+  #:use-module (ice-9 match)
+  #:use-module (common)
+  #:export (<disj> disj <conj> conj <relate> relate <==> == <mplus> mplus
+                   <bind> bind <paus> pause step mature mature?))
+
 
 ;; first-order microKanren
-(struct disj   (g1 g2)                  #:prefab)
-(struct conj   (g1 g2)                  #:prefab)
-(struct relate (thunk description)      #:prefab)
-(struct ==     (t1 t2)                  #:prefab)
-(struct bind   (bind-s bind-g)          #:prefab)
-(struct mplus  (mplus-s1 mplus-s2)      #:prefab)
-(struct pause  (pause-state pause-goal) #:prefab)
+(define-record-type <disj>
+  (disj g1 g2)
+  disj?
+  (g1 disj-g1)
+  (g2 disj-g2))
+
+(define-record-type <conj>
+  (conj g1 g2)
+  conj?
+  (g1 conj-g1)
+  (g2 conj-g2))
+
+(define-record-type <relate>
+  (relate thunk description)
+  relate?
+  (thunk relate-thunk)
+  (description relate-description))
+
+(define-record-type <==>
+  (== t1 t2)
+  ==?
+  (t1 ==-t1)
+  (t2 ==-t2))
+
+(define-record-type <bind>
+  (bind s g)
+  bind?
+  (s bind-s)
+  (g bind-g))
+
+(define-record-type <mplus>
+  (mplus s1 s2)
+  mplus?
+  (s1 mplus-s1)
+  (s2 mplus-s2))
+
+(define-record-type <pause>
+  (pause state goal)
+  pause?
+  (state pause-state)
+  (goal pause-goal))
 
 (define (mature? s) (or (not s) (pair? s)))
 (define (mature s)
@@ -29,30 +56,30 @@
 
 (define (start st g)
   (match g
-    ((disj g1 g2)
+    (($ <disj> g1 g2)
      (step (mplus (pause st g1)
                   (pause st g2))))
-    ((conj g1 g2)
+    (($ <conj> g1 g2)
      (step (bind (pause st g1) g2)))
-    ((relate thunk _)
+    (($ <relate> thunk _)
      (pause st (thunk)))
-    ((== t1 t2) (unify t1 t2 st))))
+    (($ <==> t1 t2) (unify t1 t2 st))))
 
 (define (step s)
   (match s
-    ((mplus s1 s2)
+    (($ <mplus> s1 s2)
      (let ((s1 (if (mature? s1) s1 (step s1))))
        (cond ((not s1) s2)
              ((pair? s1)
               (cons (car s1)
                     (mplus s2 (cdr s1))))
              (else (mplus s2 s1)))))
-    ((bind s g)
+    (($ <bind> s g)
      (let ((s (if (mature? s) s (step s))))
        (cond ((not s) #f)
              ((pair? s)
               (step (mplus (pause (car s) g)
                            (bind (cdr s) g))))
              (else (bind s g)))))
-    ((pause st g) (start st g))
+    (($ <pause> st g) (start st g))
     (_            s)))
