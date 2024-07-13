ef5733980ef0c1191cbd4d0c2a571a40386ca1a8
Jon Dybeck <jon@dybeck.eu>
2024-07-13 14:31:52 +0200

    Translate mk-fo.rkt into mk-fo.scm

--- mk-fo.scm
+++ mk-fo.scm
@@ -2,7 +2,6 @@
 (define-module (mk-fo)
   #:use-module (common)
   #:use-module (microk-fo)
-  #:re-export (== mk-pause)
-  #:export (fresh define-relation conde query run run* stream-take conj*
-               disj*))
+  #:re-export (== =/= symbolo numbero stringo not-symbolo not-numbero not-stringo mk-pause)
+  #:export (define-relation fresh conde query run run* stream-take conj* disj*))
 (include "mk-syntax.scm")
